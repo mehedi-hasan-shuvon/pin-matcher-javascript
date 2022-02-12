e332b7056979e88a42d35beff600925fbb8a7ea5
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-02-13 02:14:13 +0600

    typing inputs

--- js/app.js
+++ js/app.js
@@ -13,4 +13,23 @@ function getPin() {
 function generatePin() {
     const pin = getPin()
     console.log(pin);
+    document.getElementById('display-pin').value = pin;
 }
+
+document.getElementById('key-pad').addEventListener('click', function (event) {
+    const number = event.target.innerText;
+    const calcInput = document.getElementById('typed-numbers');
+    if (isNaN(number)) {
+        console.log(number);
+        if (number == 'C') {
+            calcInput.value = '';
+        } else if (number == '<') {
+            calcInput.value = calcInput.value.slice(0, -1);
+        }
+    } else {
+
+        const previousNumber = calcInput.value;
+        const newNumber = previousNumber + number;
+        calcInput.value = newNumber;
+    }
+})
